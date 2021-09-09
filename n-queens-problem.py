5e8397c82d9ff2c5045196ab4e6ad506af31bd47
bobbyinvents <72687709+bobbyinvents@users.noreply.github.com>
2021-09-09 00:36:23 +0000

    Update n-queens-problem.py

--- n-queens-problem.py
+++ n-queens-problem.py
@@ -1,31 +1,45 @@
+from enum import Enum
 import string
 
-# Symbols on the chessboard
-QUEEN_PIECE = "Q"
-EMPTY_SPACE = "."
-ATTACK_SPACE = "X"
-TAKEN_SPACE = "o"
-EM_DASH = "—"
+
+class BoardSymbols(Enum):
+    QUEEN_PIECE = "Q"
+    EMPTY_SPACE = "."
+    ATTACK_SPACE = "X"
+    TAKEN_SPACE = "o"
+    EM_DASH = "—"
 
 
-def display_board(board: list) -> list:
+def create_empty_board(n: int) -> list:
+    """Create an empty matrix that represents a chessboard.
+    n will define the length and width of the chessboard and the total number of queens."""
+    board_length = board_width = n
+    return [[BoardSymbols.EMPTY_SPACE] * board_length for _ in range(board_width)]
+
+
+def display_board(board: list):
     """Print out the chessboard in an easy to understand way."""
     n = len(board)
-    print(" " * (len(str(n)) + 3) + " ".join(f"{string.ascii_lowercase[:n]}"))
-    print(" ".join(EM_DASH * n).rjust(len(str(n)) + n * 2 + 2))
-    print(
-        "\n".join(
-            f"{n - row_index} | ".rjust(len(str(n)) + 3)
-            + " ".join("♛" if piece == QUEEN_PIECE else "." for piece in row)
-            + f" | {n - row_index}"
-            for row_index, row in enumerate(board)
-        )
+    n_str_length = len(str(n))
+
+    files = " " * (n_str_length + 3) + " ".join(f"{string.ascii_lowercase[:n]}")
+    horizontal_line = " ".join(BoardSymbols.EM_DASH.value * n).rjust(
+        n_str_length + n * 2 + 2
     )
-    print(" ".join(EM_DASH * n).rjust(len(str(n)) + n * 2 + 2))
-    print(" " * (len(str(n)) + 3) + " ".join(f"{string.ascii_lowercase[:n]}"))
+    print(files, horizontal_line, sep="\n")
+
+    for row_index, row in enumerate(board):
+        ranks = f"{n - row_index} | ".rjust(n_str_length + 3)
+        right_side_ranks = ranks[-3:] + ranks[:-3].strip()
+        pieces = " ".join(
+            "♛" if piece == BoardSymbols.QUEEN_PIECE else "." for piece in row
+        )
+        print(ranks + pieces + right_side_ranks)
+
+    print(horizontal_line, files, sep="\n")
 
 
-def get_positions(piece: str, board: list) -> list:
+def get_positions(piece: BoardSymbols, board: list) -> list:
     """Get a list of positions on the chessboard that the piece of interest can be found."""
     return [
         [row_index, col_index]
@@ -42,32 +56,34 @@ def place_queen(board: list) -> list:
         [row_index, col_index]
         for row_index, row_value in enumerate(board)
         for col_index, col_value in enumerate(row_value)
-        if col_value == EMPTY_SPACE
+        if col_value == BoardSymbols.EMPTY_SPACE
     )
 
-    board[free_row][free_col] = QUEEN_PIECE
+    board[free_row][free_col] = BoardSymbols.QUEEN_PIECE
     new_board = [[col for col in row] for row in board]
-    board[free_row][free_col] = TAKEN_SPACE
+    board[free_row][free_col] = BoardSymbols.TAKEN_SPACE
     taken_board = [[col for col in row] for row in board]
     return new_board, taken_board
 
 
 def can_place_queen(board: list) -> bool:
     """Checks if the chessboard has a valid spot for a queen."""
-    board = mark_attacks(board)
-    valid_position = any(piece == EMPTY_SPACE for row in board for piece in row)
-    return valid_position
+    return any(
+        piece == BoardSymbols.EMPTY_SPACE
+        for row in mark_attacks(board)
+        for piece in row
+    )
 
 
 def mark_attacks(board: list) -> list:
     """Mark position on the board that can be attacked by a queen."""
-    queen_positions = get_positions(QUEEN_PIECE, board)
+    queen_positions = get_positions(BoardSymbols.QUEEN_PIECE, board)
     n = len(board)
     for queen_row, queen_col in queen_positions:
         board = [
             [
-                ATTACK_SPACE
-                if board[row][col] != QUEEN_PIECE
+                BoardSymbols.ATTACK_SPACE
+                if board[row][col] != BoardSymbols.QUEEN_PIECE
                 and (
                     row == queen_row
                     or col == queen_col
@@ -94,13 +110,14 @@ def get_valid_boards(n: int) -> list:
         # no chessboard, so no valid ways
         return []
     else:
-        return get_valid_boards_helper(n, create_board(n))
+        return get_valid_boards_helper(n, create_empty_board(n))
 
 
 def get_valid_boards_helper(n_queens: int, board: list) -> list:
-    """Gets a list of all valid n by n chessboard with n queens 
+    """Gets a list of all valid n by n chessboard with n queens
     placed in such a way that no two queens attack each other."""
     if n_queens == 0:
+        write_to_output_file(f"{get_positions(BoardSymbols.QUEEN_PIECE, board)}\n")
         return [board]
     elif can_place_queen(board):
         new_board, taken_board = place_queen(board)
@@ -111,15 +128,23 @@ def get_valid_boards_helper(n_queens: int, board: list) -> list:
     return []
 
 
-def create_board(n: int) -> list:
-    """Create an empty matrix that represents a chessboard.
-    n will define the length and width of the chessboard and the total number of queens."""
-    board_length = board_width = n
-    empty_board = [[EMPTY_SPACE] * board_length for _ in range(board_width)]
-    return empty_board
+def display_example_board():
+    board = create_empty_board(8)
+    queen_positions = [[0, 7], [1, 3], [2, 0], [3, 2], [4, 5], [5, 1], [6, 6], [7, 4]]
+    for x, y in queen_positions:
+        board[x][y] = BoardSymbols.QUEEN_PIECE
+    display_board(board)
+
+
+def write_to_output_file(string: str):
+    with open("output.txt", "a") as o:
+        o.write(string)
 
 
 if __name__ == "__main__":
+    print("THE N QUEENS PUZZLE")
+    display_example_board()
+
     print(
         "This program will solve the puzzle of placing n queens on "
         "an n by n chessboard such that no queens can attack each other!"
@@ -127,7 +152,9 @@ if __name__ == "__main__":
     n = int(input("""Enter an integer n: """))
     print("""Getting all distinct solutions...\n""")
 
+    write_to_output_file(f"{'='*20}\nSOLUTIONS FOR n={n}\n{'='*20}\n")
     valid_boards = get_valid_boards(n)
+    write_to_output_file("\n\n\n\n")
     if not valid_boards:
         print("There are no possible solutions!")
 
@@ -135,3 +162,5 @@ if __name__ == "__main__":
         print(f"Solution {board_index+1}")
         display_board(board)
         print()
+
+    print("Solutions have been saved to output.txt.")
